2064836d02847a2dfe9298e931c2d21878de2d39
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2025-07-25 14:09:13 +0100

    FLP-1280 AddKeyToGetLearnings

--- src/Queries/GetApprenticeshipsByAcademicYear/GetLearningsByDatesResponseItem.cs
+++ src/Queries/GetApprenticeshipsByAcademicYear/GetLearningsByDatesResponseItem.cs
@@ -3,4 +3,5 @@ namespace SFA.DAS.Learning.Queries.GetApprenticeshipsByAcademicYear;
 public record GetLearningsByDatesResponseItem
 {
     public string Uln { get; init; }
+    public Guid Key { get; set; }
 }
