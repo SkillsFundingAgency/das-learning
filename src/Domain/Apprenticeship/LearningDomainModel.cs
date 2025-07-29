6333b72f2d3236c120f64bdcb99255f62e3433a7
tom-gough <tom.gough.tgsd@outlook.com>
2025-07-29 13:16:16 +0100

    FLP-1269 fixed comparison of courses

--- src/Domain/Apprenticeship/LearningDomainModel.cs
+++ src/Domain/Apprenticeship/LearningDomainModel.cs
@@ -394,7 +394,7 @@ public class LearningDomainModel : AggregateRoot
     {
         foreach (var course in updateModel.MathsAndEnglishCourses)
         {
-            var existingCourse = _entity.MathsAndEnglishCourses.SingleOrDefault(x => x.Course == course.Course);
+            var existingCourse = _entity.MathsAndEnglishCourses.SingleOrDefault(x => x.Course.Trim() == course.Course.Trim());
 
             if (existingCourse == null)
             {
@@ -428,7 +428,7 @@ public class LearningDomainModel : AggregateRoot
             .Select(c => c.Course);
 
         var coursesToRemove = _entity.MathsAndEnglishCourses
-            .Where(existing => !incomingCourses.Contains(existing.Course))
+            .Where(existing => !incomingCourses.Any(incoming => incoming.Trim() == existing.Course.Trim()))
             .ToList();
 
         foreach (var removed in coursesToRemove)
