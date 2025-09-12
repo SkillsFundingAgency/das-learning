275852d40bd0b1e6f15fc0ee694c074a53d47839
Chris Foster <chrisfoster186@googlemail.com>
2025-09-12 15:16:34 +0100

    Adds sql script

--- src/Database/Scripts/PostDeployment.sql
+++ src/Database/Scripts/PostDeployment.sql
@@ -2,7 +2,12 @@
 Post-deployment script
 */
 
-IF OBJECT_ID('[dbo].[Apprenticeship]', 'U') IS NOT NULL
+IF OBJECT_ID('[dbo].[StartDateChange]', 'U') IS NOT NULL
 BEGIN
-    DROP TABLE [dbo].[Apprenticeship];
+    DROP TABLE [dbo].StartDateChange;
+END
+
+IF OBJECT_ID('[dbo].[PriceHistory]', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE [dbo].PriceHistory;
 END
