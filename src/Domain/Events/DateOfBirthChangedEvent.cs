e236e7872551861d6c11e859fb13fdb45e0ad976
tom-gough <tom.gough@talentconsulting.uk>
2025-12-01 08:55:43 +0000

    FLP-1239 DOB Change Learning

---
+++ src/Domain/Events/DateOfBirthChangedEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.Learning.Domain.Events;
+
+public class DateOfBirthChangedEvent : IDomainEvent
+{
+    public Guid LearningKey { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
