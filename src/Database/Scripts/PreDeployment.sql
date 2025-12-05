fd92cdc9b469ea039a4304d9cd017283ebd720bb
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2025-12-05 12:55:57 +0000

    Update PreDeployment.sql

--- src/Database/Scripts/PreDeployment.sql
+++ src/Database/Scripts/PreDeployment.sql
@@ -1,25 +1,3 @@
 ﻿/*
 Pre-deployment script
 */
-
-IF EXISTS (
-    SELECT 1
-    FROM sys.columns
-    WHERE Name = N'FundingBandMaximum'
-      AND Object_ID = Object_ID(N'dbo.EpisodePrice')
-)
-BEGIN
-    ALTER TABLE dbo.EpisodePrice
-    DROP COLUMN FundingBandMaximum;
-END
-
-IF EXISTS (
-    SELECT 1
-    FROM sys.columns
-    WHERE Name = N'FundingBandMaximum'
-      AND Object_ID = Object_ID(N'dbo.Episode')
-)
-BEGIN
-    ALTER TABLE dbo.Episode
-    DROP COLUMN FundingBandMaximum;
-END
