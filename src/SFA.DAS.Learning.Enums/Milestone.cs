e9476a83c20458957f4de0dadbfd2235ae7ced8f
tom-gough <tom.gough@talentconsulting.uk>
2026-01-30 09:58:19 +0000

    FLP-1527 added real values for Milestones on short courses

--- src/SFA.DAS.Learning.Enums/Milestone.cs
+++ src/SFA.DAS.Learning.Enums/Milestone.cs
@@ -1,10 +1,7 @@
 ﻿namespace SFA.DAS.Learning.Enums;
 
-//todo these values are placeholders for the spike and need to be replaced with the correct ones
 public enum Milestone
 {
-    Enrolled = 1,
-    Started = 2,
-    Completed = 3,
-    Certified = 4
+    ThirtyPercentLearningComplete = 1,
+    LearningComplete = 2,
 }
