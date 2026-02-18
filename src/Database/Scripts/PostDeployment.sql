5e91c8b010507140cb624b212d06a5d18cf8287e
Chris Foster <chrisfoster186@googlemail.com>
2026-02-18 08:48:16 +0000

    no message

--- src/Database/Scripts/PostDeployment.sql
+++ src/Database/Scripts/PostDeployment.sql
@@ -5,6 +5,7 @@ Post-deployment script
 
 --FLP-1493 - Data Migration
 
+--Populate Learner table
 ;WITH Deduped AS
           (
               SELECT
@@ -41,15 +42,18 @@ SELECT
     HasEHCP,
     IsCareLeaver,
     CareLeaverEmployerConsentGiven
-FROM Deduped
-WHERE rn = 1;
-GO
+FROM Deduped d
+WHERE d.rn = 1
+  AND NOT EXISTS (
+    SELECT 1
+    FROM dbo.Learner lr
+    WHERE lr.Uln = d.Uln
+);
 
+--Update Learning table with LearnerKey
 UPDATE L
 SET L.LearnerKey = LR.[Key]
 FROM dbo.Learning AS L
     INNER JOIN dbo.Learner AS LR
-ON L.Uln = LR.Uln;
-GO
-
-
+ON L.Uln = LR.Uln
+WHERE L.LearnerKey IS NULL;
