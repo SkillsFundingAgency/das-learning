007c36b9433a5540e2519d408668210bfaed9faa
tom-gough <tom.gough@talentconsulting.uk>
2026-02-24 12:07:13 +0000

    FLP-1493 tidy up

--- src/Database/Scripts/Adhoc/FLP-1493-PreDeployment-DeleteAllData.sql
+++ src/Database/Scripts/Adhoc/FLP-1493-PreDeployment-DeleteAllData.sql
@@ -1,35 +1,21 @@
--- Disable referential integrity to avoid foreign key constraint errors
-ALTER TABLE [History].[LearningHistory] NOCHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[MathsAndEnglishBreakInLearning] NOCHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[MathsAndEnglish] NOCHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[EpisodeBreakInLearning] NOCHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[EpisodePrice] NOCHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[Episode] NOCHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[LearningSupport] NOCHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[FreezeRequest] NOCHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[Learning] NOCHECK CONSTRAINT ALL;
-GO
+-- Script to clear all data from the database
 
--- Delete all data in the tables
-DELETE FROM [History].[LearningHistory];
+BEGIN TRAN
+
 DELETE FROM [dbo].[MathsAndEnglishBreakInLearning];
-DELETE FROM [dbo].[MathsAndEnglish];
 DELETE FROM [dbo].[EpisodeBreakInLearning];
 DELETE FROM [dbo].[EpisodePrice];
-DELETE FROM [dbo].[Episode];
 DELETE FROM [dbo].[LearningSupport];
-DELETE FROM [dbo].[FreezeRequest];
-DELETE FROM [dbo].[Learning];
-GO
 
--- Re-enable referential integrity
-ALTER TABLE [History].[LearningHistory] WITH CHECK CHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[MathsAndEnglishBreakInLearning] WITH CHECK CHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[MathsAndEnglish] WITH CHECK CHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[EpisodeBreakInLearning] WITH CHECK CHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[EpisodePrice] WITH CHECK CHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[Episode] WITH CHECK CHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[LearningSupport] WITH CHECK CHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[FreezeRequest] WITH CHECK CHECK CONSTRAINT ALL;
-ALTER TABLE [dbo].[Learning] WITH CHECK CHECK CONSTRAINT ALL;
+DELETE FROM [dbo].[MathsAndEnglish];
+DELETE FROM [dbo].[Episode];
+
+DELETE FROM [dbo].[FreezeRequest];
+DELETE FROM [History].[LearningHistory];
+
+DELETE FROM [dbo].[Learning];
+
+ROLLBACK TRAN
+--COMMIT TRAN
+
 GO
