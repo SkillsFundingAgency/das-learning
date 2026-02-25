9427378349eaabddf28217771dde2123d0153dc6
tom-gough <tom.gough@talentconsulting.uk>
2026-02-25 11:46:48 +0000

    FLP-1493 pre-deployment script updated

--- src/Database/Scripts/PreDeployment.sql
+++ src/Database/Scripts/PreDeployment.sql
@@ -1,3 +1,18 @@
 ﻿/*
 Pre-deployment script
 */
+
+BEGIN TRAN
+
+    DELETE FROM [dbo].[MathsAndEnglishBreakInLearning];
+    DELETE FROM [dbo].[EpisodeBreakInLearning];
+    DELETE FROM [dbo].[EpisodePrice];
+
+    DELETE FROM [dbo].[MathsAndEnglish];
+    DELETE FROM [dbo].[Episode];
+
+    DELETE FROM [History].[LearningHistory];
+
+    DELETE FROM [dbo].[Learning];
+
+COMMIT TRAN
