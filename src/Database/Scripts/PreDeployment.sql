e2af8e21827c11129ec965d81d2feb93f23c8e30
Chris Foster <chrisfoster186@googlemail.com>
2026-03-09 09:20:01 +0000

    Removes scripts

--- src/Database/Scripts/PreDeployment.sql
+++ src/Database/Scripts/PreDeployment.sql
@@ -1,30 +1,3 @@
 ﻿/*
 Pre-deployment script
 */
-
-IF EXISTS (
-    SELECT 1
-    FROM sys.tables t
-    INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
-    WHERE t.name = 'Episode' -- check that one of the tables removed in FLP-1493 exists in the db
-      AND s.name = 'dbo'
-)
-BEGIN
-    BEGIN TRAN
-
-    DELETE FROM [dbo].[MathsAndEnglishBreakInLearning];
-    DELETE FROM [dbo].[EpisodeBreakInLearning];
-    DELETE FROM [dbo].[EpisodePrice];
-    DELETE FROM [dbo].[LearningSupport];
-
-    DELETE FROM [dbo].[MathsAndEnglish];
-    DELETE FROM [dbo].[Episode];
-
-	DELETE FROM [dbo].[FreezeRequest]
-    DELETE FROM [History].[LearningHistory];
-
-    DELETE FROM [dbo].[Learning];
-
-    COMMIT TRAN
-END
-GO
