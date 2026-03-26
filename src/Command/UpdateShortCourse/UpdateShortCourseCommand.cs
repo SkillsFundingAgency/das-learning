90ac9c488a9d60200a85050ab695bc82ff1bfbb5
Chris Foster <chrisfoster186@googlemail.com>
2026-03-26 10:36:17 +0000

    WIP

--- src/Command/UpdateShortCourse/UpdateShortCourseCommand.cs
+++ src/Command/UpdateShortCourse/UpdateShortCourseCommand.cs
@@ -19,4 +19,28 @@ public class UpdateShortCourseResult
 {
     public Guid LearningKey { get; set; }
     public ShortCourseUpdateChanges[] Changes { get; set; } = [];
+    public UpdateShortCourseResultLearner Learner { get; set; } = null!;
+    public UpdateShortCourseResultEpisode[] Episodes { get; set; } = [];
+}
+
+public class UpdateShortCourseResultLearner
+{
+    public string Uln { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public DateTime DateOfBirth { get; set; }
+}
+
+public class UpdateShortCourseResultEpisode
+{
+    public long Ukprn { get; set; }
+    public string CourseCode { get; set; } = null!;
+    public string CourseType { get; set; } = null!;
+    public LearningType LearningType { get; set; }
+    public DateTime StartDate { get; set; }
+    public int AgeAtStart { get; set; }
+    public DateTime PlannedEndDate { get; set; }
+    public DateTime? WithdrawalDate { get; set; }
+    public bool IsApproved { get; set; }
+    public string LearnerRef { get; set; } = null!;
 }
