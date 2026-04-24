0567600cd18eec10d9a0a5a7da9d5957b6f90292
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2026-04-24 15:36:53 +0100

    Update PreDeployment.sql

--- src/Database/Scripts/PreDeployment.sql
+++ src/Database/Scripts/PreDeployment.sql
@@ -2,10 +2,11 @@
 Pre-deployment script
 */
 
+
+-- FLP-1289 (delete this script after 1289 deployed to prod)
 IF OBJECT_ID('[Domain].[EnglishAndMaths]', 'U') IS NULL
     RETURN;
 
--- EmployerAccountId
 IF COL_LENGTH('[Domain].[EnglishAndMaths]', 'PriorLearningPercentage') IS NOT NULL
 BEGIN
     EXEC('UPDATE [Domain].[EnglishAndMaths]
