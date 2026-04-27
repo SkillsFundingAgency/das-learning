0d1ae1e03f5c921a7886f5806b0957f9d8cd195a
Chris Foster <chrisfoster186@googlemail.com>
2026-04-27 16:16:37 +0100

    Adds (unused) reinstatement event

--- src/Types/LearningReinstatedEvent.cs
+++ src/Types/LearningReinstatedEvent.cs
@@ -1,6 +1,6 @@
 namespace SFA.DAS.Learning.Types;
 
-public class LearningRemovedEvent
+public class LearningReinstatedEvent
 {
     public Guid LearningKey { get; set; }
     public long ApprenticeshipId { get; set; }
