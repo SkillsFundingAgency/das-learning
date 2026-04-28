3c387b75c9ae9fc0470209332068e26a231a27af
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2026-04-28 11:01:37 +0100

    Update PreDeployment.sql

--- src/Database/Scripts/PreDeployment.sql
+++ src/Database/Scripts/PreDeployment.sql
@@ -2,21 +2,22 @@
 Pre-deployment script
 */
 
-IF OBJECT_ID(N'[dbo].[Approval]', N'U') IS NOT NULL
-    BEGIN
-        DROP TABLE [dbo].[Approval];
-    END
-GO
-
 IF OBJECT_ID(N'[dbo].[ClientOutboxData]', N'U') IS NOT NULL
     BEGIN
         DROP TABLE [dbo].[ClientOutboxData];
     END
 GO
 
-IF OBJECT_ID(N'[dbo].[Episode]', N'U') IS NOT NULL
+IF OBJECT_ID(N'[dbo].[OutboxData]', N'U') IS NOT NULL
     BEGIN
-        DROP TABLE [dbo].[Episode];
+        DROP TABLE [dbo].[OutboxData];
+    END
+GO
+
+
+IF OBJECT_ID(N'[dbo].[Approval]', N'U') IS NOT NULL
+    BEGIN
+        DROP TABLE [dbo].[Approval];
     END
 GO
 
@@ -26,21 +27,21 @@ IF OBJECT_ID(N'[dbo].[FreezeRequest]', N'U') IS NOT NULL
     END
 GO
 
-IF OBJECT_ID(N'[dbo].[Learning]', N'U') IS NOT NULL
-    BEGIN
-        DROP TABLE [dbo].[Learning];
-    END
-GO
-
 IF OBJECT_ID(N'[dbo].[LearningSupport]', N'U') IS NOT NULL
     BEGIN
         DROP TABLE [dbo].[LearningSupport];
     END
 GO
 
-IF OBJECT_ID(N'[dbo].[MathsAndEnglish]', N'U') IS NOT NULL
+IF OBJECT_ID(N'[dbo].[Episode]', N'U') IS NOT NULL
     BEGIN
-        DROP TABLE [dbo].[MathsAndEnglish];
+        DROP TABLE [dbo].[Episode];
+    END
+GO
+
+IF OBJECT_ID(N'[dbo].[Learning]', N'U') IS NOT NULL
+    BEGIN
+        DROP TABLE [dbo].[Learning];
     END
 GO
 
@@ -50,8 +51,9 @@ IF OBJECT_ID(N'[dbo].[MathsAndEnglishBreakInLearning]', N'U') IS NOT NULL
     END
 GO
 
-IF OBJECT_ID(N'[dbo].[OutboxData]', N'U') IS NOT NULL
+IF OBJECT_ID(N'[dbo].[MathsAndEnglish]', N'U') IS NOT NULL
     BEGIN
-        DROP TABLE [dbo].[OutboxData];
+        DROP TABLE [dbo].[MathsAndEnglish];
     END
 GO
+
