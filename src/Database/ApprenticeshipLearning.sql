8b30ec6600d2a54353e959a52489aa837d30d40a
tom-gough <tom.gough@talentconsulting.uk>
2026-05-21 14:07:24 +0100

    FLP-1377 merge fix

--- src/Database/ApprenticeshipLearning.sql
+++ src/Database/ApprenticeshipLearning.sql
@@ -2,9 +2,8 @@ CREATE TABLE [dbo].[ApprenticeshipLearning]
 (
     [Key] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
     [LearnerKey] UNIQUEIDENTIFIER NOT NULL,
-    [ApprovalsApprenticeshipId] BIGINT NOT NULL,
     [CompletionDate] DATETIME NULL,
-    [AchievementDate] DATETIME NULL,
+    [AchievementDate] DATETIME NULL
 )
     GO
 CREATE INDEX IX_ApprenticeshipLearning_LearnerKey ON ApprenticeshipLearning (LearnerKey)
