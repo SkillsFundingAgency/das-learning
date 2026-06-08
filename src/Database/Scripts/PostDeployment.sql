013811f9861cd4f3effec0c2fc7de904e8c84998
Chris Foster <chrisfoster186@googlemail.com>
2026-06-08 16:13:32 +0100

    Simplifies post deployment script

--- src/Database/Scripts/PostDeployment.sql
+++ src/Database/Scripts/PostDeployment.sql
@@ -5,14 +5,10 @@
 -- FLP-1868 (delete this script once CompletionDate is dropped from ShortCourseLearning)
 -- Backfill ShortCourseEpisode.CompletionDate from ShortCourseLearning.CompletionDate.
 IF COL_LENGTH('[dbo].[ShortCourseEpisode]', 'CompletionDate') IS NOT NULL
-   AND COL_LENGTH('[dbo].[ShortCourseLearning]', 'CompletionDate') IS NOT NULL
 BEGIN
-    UPDATE [sce]
-    SET [sce].[CompletionDate] = [scl].[CompletionDate]
-    FROM [dbo].[ShortCourseEpisode] AS [sce]
-    INNER JOIN [dbo].[ShortCourseLearning] AS [scl]
-        ON [scl].[Key] = [sce].[LearningKey]
-    WHERE [scl].[CompletionDate] IS NOT NULL
-      AND [sce].[IsRemoved] = 0
-      AND [sce].[CompletionDate] IS NULL
+	UPDATE [sce]
+	SET [sce].[CompletionDate] = [scl].[CompletionDate]
+	FROM [dbo].[ShortCourseEpisode] AS [sce]
+	INNER JOIN [dbo].[ShortCourseLearning] AS [scl]
+	ON [scl].[Key] = [sce].[LearningKey]
 END
