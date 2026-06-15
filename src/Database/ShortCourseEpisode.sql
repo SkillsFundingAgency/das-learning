ff4cffca664912b2d1ec1839137d7ad4eabe209b
Chris Foster <chrisfoster186@googlemail.com>
2026-06-15 12:28:39 +0100

    Adds default for trainingcode on Episode for post-change scenario

--- src/Database/ShortCourseEpisode.sql
+++ src/Database/ShortCourseEpisode.sql
@@ -7,7 +7,7 @@ CREATE TABLE [dbo].[ShortCourseEpisode]
     [StartDate] DATETIME NOT NULL,
     [ExpectedEndDate] DATETIME NOT NULL,
     [WithdrawalDate] DATETIME NULL,
-    [TrainingCode] VARCHAR(8) NOT NULL,
+    [TrainingCode] VARCHAR(8) NOT NULL DEFAULT '',
     [ApprovalsApprenticeshipId] BIGINT NOT NULL DEFAULT 0,
     [IsApproved] BIT NOT NULL DEFAULT 0,
     [Price] MONEY NOT NULL DEFAULT 0,
