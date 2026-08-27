0dc3187f955b467bf240d06507077e3d65fd31f5
Chris Foster <chrisfoster186@googlemail.com>
2026-08-27 12:40:46 +0100

    Removes backfill script as no data to migrate in prod

--- src/Database/Scripts/PostDeployment.sql
+++ src/Database/Scripts/PostDeployment.sql
@@ -15,18 +15,3 @@ BEGIN
 	ON [ae].[LearningKey] = [al].[Key]
 	WHERE [al].[TrainingCode] = ''
 END
-
--- FLP-2068 Backfill CompletionDate/AchievementDate from learning to episode. (todo delete this and drop legacy columns [CompletionDate], [AchievementDate] from ApprenticeshipLearning once all environments have migrated data to ApprenticeshipEpisode)
--- Assumes 1:1 Apprenticeship -> Episode relationship
-IF COL_LENGTH('[dbo].[ApprenticeshipEpisode]', 'CompletionDate') IS NOT NULL
-   AND COL_LENGTH('[dbo].[ApprenticeshipLearning]', 'CompletionDate') IS NOT NULL
-BEGIN
-	UPDATE [ae]
-	SET [ae].[CompletionDate] = [al].[CompletionDate],
-		[ae].[AchievementDate] = [al].[AchievementDate]
-	FROM [dbo].[ApprenticeshipEpisode] AS [ae]
-	INNER JOIN [dbo].[ApprenticeshipLearning] AS [al]
-	ON [al].[Key] = [ae].[LearningKey]
-	WHERE [ae].[CompletionDate] IS NULL AND [al].[CompletionDate] IS NOT NULL
-	   OR [ae].[AchievementDate] IS NULL AND [al].[AchievementDate] IS NOT NULL
-END
