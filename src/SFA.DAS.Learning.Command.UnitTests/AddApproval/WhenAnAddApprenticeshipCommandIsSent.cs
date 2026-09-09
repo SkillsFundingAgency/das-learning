7fc676096b7b405dcb2ecc23f1efae03b652923b
Chris Foster <chrisfoster186@googlemail.com>
2026-09-09 13:48:38 +0100

    Adds test coverage

--- src/SFA.DAS.Learning.Command.UnitTests/AddApproval/WhenAnAddApprenticeshipCommandIsSent.cs
+++ src/SFA.DAS.Learning.Command.UnitTests/AddApproval/WhenAnAddApprenticeshipCommandIsSent.cs
@@ -207,6 +207,30 @@ public class WhenAnAddApprenticeshipCommandIsSent
     }
 
     [Test]
+    public async Task WhenAnUnapprovedShortCourseExistsThenAShortCourseLearningChangedEventIsRaisedWithApprovedOperation()
+    {
+        var command = _fixture.Build<AddLearningCommand>()
+            .With(x => x.LearningType, LearningType.ApprenticeshipUnit)
+            .Create();
+
+        var shortCourseLearning = _fixture.Create<ShortCourseLearningDomainModel>();
+
+        _learningService.Setup(x => x.GetUnapprovedLearning(command.Uln, LearningType.ApprenticeshipUnit, It.IsAny<long>(), It.IsAny<string>()))
+            .ReturnsAsync(shortCourseLearning);
+
+        command.UKPRN = shortCourseLearning.Episodes.First().Ukprn;
+        await _commandHandler.Handle(command);
+
+        shortCourseLearning
+            .FlushEvents()
+            .OfType<Domain.Events.ShortCourseLearningChangedEvent>()
+            .Should()
+            .ContainSingle(e => e.LearningKey == shortCourseLearning.Key
+                                 && e.AcademicYear == null
+                                 && e.Operation == ShortCourseLearningOperation.Approved);
+    }
+
+    [Test]
     public async Task WhenAnUnapprovedShortCourseExistsThenTheEmployerAccountIdIsUpdated()
     {
         var command = _fixture.Build<AddLearningCommand>()
